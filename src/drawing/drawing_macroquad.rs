760ef283ab9d2df4f6b4754a9ede4e5f966a02e0
jose miguel mut <jomutlo@gmail.com>
2022-06-02 14:20:13 +0100

    attempt to extract the main factory

--- src/drawing/drawing_macroquad.rs
+++ src/drawing/drawing_macroquad.rs
@@ -1,14 +1,14 @@
 use macroquad::color::colors::GRAY;
-use macroquad::color::{BLACK, Color};
+use macroquad::color::{Color, BLACK};
 use macroquad::miniquad::date::now;
 use macroquad::prelude::Texture2D;
 use macroquad::text::draw_text;
 use macroquad::texture::draw_texture;
 use macroquad::window::{clear_background, screen_width};
 
+use super::super::game_state::GameState;
 use super::assets::load_tileset;
 use super::DrawingTrait;
-use super::super::game_state::GameState;
 
 pub struct DrawingMacroquad {
     pub textures: Vec<Texture2D>,
@@ -33,9 +33,15 @@ impl DrawingTrait for DrawingMacroquad {
         // draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         // draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
         //
-        let text = format!("{:.0}", 1.0/(now() - game_state.previous_frame_ts));
+        let text = format!("{:.0}", 1.0 / (now() - game_state.previous_frame_ts));
         let font_size = 30.0;
-        draw_text(text.as_str(), screen_width() - font_size *2.0, 20.0, font_size, BLACK);
+        draw_text(
+            text.as_str(),
+            screen_width() - font_size * 2.0,
+            20.0,
+            font_size,
+            BLACK,
+        );
         draw_texture(self.textures[1], 0.0, 0.0, Color::new(1.0, 1.0, 1.0, 1.0));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
 mod drawing;
-mod input;
 mod game_state;
+mod input;
 
 use macroquad::prelude::next_frame;
 
 use drawing::drawing_macroquad::DrawingMacroquad;
 use drawing::DrawingTrait;
+use game_state::GameState;
 use input::input_macroquad::InputMacroquad as InputSource;
 use input::InputSourceTrait;
-use game_state::GameState;
+
+struct Implementations<D: DrawingTrait> {
+    drawer: D,
+    game_state: GameState,
+}
 
 #[macroquad::main("Bioengineer")]
 async fn main() {
-    let drawer = DrawingMacroquad::new("assets/image/tileset.png");
-    let mut game_state = GameState::new();
+    let Implementations {
+        drawer,
+        mut game_state,
+    } = factory();
+
     while frame(&mut game_state, &drawer) {
         next_frame().await
     }
 }
 
-/// returns if should continue looping
+fn factory() -> Implementations<DrawingMacroquad> {
+    let drawer = DrawingMacroquad::new("assets/image/tileset.png");
+    let game_state = GameState::new();
+    Implementations { drawer, game_state }
+}
+
+/// returns if should continue looping. In other words, if there should be another future frame.
 fn frame(game_state: &mut GameState, drawer: &impl DrawingTrait) -> bool {
     let input = InputSource::get_input();
     if !input.quit {
