610378e896776f56ab7eeea74a701f0d3891937a
jose miguel mut <jomutlo@gmail.com>
2022-06-06 21:09:48 +0100

    fix wheel in wasm

--- src/external/input_macroquad.rs
+++ src/external/input_macroquad.rs
@@ -46,17 +46,28 @@ impl InputMacroquad {
             Option::None
         }
     }
-}
 
-impl InputSourceTrait for InputMacroquad {
-    fn get_input(&mut self) -> Input {
-        let quit = is_key_down(KeyCode::Escape);
+    fn get_mouse_wheel_height_diff(&mut self) -> i32 {
         let (mouse_x, mouse_y) = mouse_wheel();
         if mouse_x != 0.0 || mouse_y != 0.0 {
             // TODO how can I log in the js console_log?
             // eprintln!("mouse wheel: {}, {}", mouse_x, mouse_y);
         }
-        let change_height_rel = mouse_y as i32;
+        let change_height_rel = if mouse_y > 0.0 {
+            1
+        } else if mouse_y < 0.0 {
+            -1
+        } else {
+            0
+        };
+        change_height_rel
+    }
+}
+
+impl InputSourceTrait for InputMacroquad {
+    fn get_input(&mut self) -> Input {
+        let quit = is_key_down(KeyCode::Escape);
+        let change_height_rel = self.get_mouse_wheel_height_diff();
         let move_map_horizontally = self.get_horizontal_move();
         let start_selection = self.get_left_click_position();
         let end_selection = self.get_left_click_release();
