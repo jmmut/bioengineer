518fd4515862a5daa9f733315d8556607a705124
jose miguel mut <jomutlo@gmail.com>
2022-06-06 21:47:20 +0100

    set minification/magnification filter to Nearest

--- src/external/assets_macroquad.rs
+++ src/external/assets_macroquad.rs
@@ -1,5 +1,5 @@
 use crate::drawing::assets::{extract_images, PIXELS_PER_TILE_HEIGHT, PIXELS_PER_TILE_WIDTH};
-use macroquad::texture::Texture2D;
+use macroquad::texture::{FilterMode, Texture2D};
 
 pub async fn load_tileset(path: &str) -> Vec<Texture2D> {
     let image_future = macroquad::texture::load_image(path);
@@ -7,6 +7,10 @@ pub async fn load_tileset(path: &str) -> Vec<Texture2D> {
     let images = extract_images(&image, PIXELS_PER_TILE_WIDTH, PIXELS_PER_TILE_HEIGHT);
     images
         .into_iter()
-        .map(|image| Texture2D::from_rgba8(image.width, image.height, &image.bytes))
+        .map(|image| {
+            let texture = Texture2D::from_rgba8(image.width, image.height, &image.bytes);
+            texture.set_filter(FilterMode::Nearest);
+            texture
+        })
         .collect()
 }
