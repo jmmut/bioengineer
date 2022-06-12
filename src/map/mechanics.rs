6c42fd9f9f321361982f95a47f3fa0a5a9037b1d
jose miguel mut <jomutlo@gmail.com>
2022-06-12 22:44:17 +0100

    implement set_intersection

--- src/map/mechanics.rs
+++ src/map/mechanics.rs
@@ -2,7 +2,7 @@ use crate::map::{Cell, CellIndex, TileType};
 use crate::GameState;
 use std::collections::HashSet;
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
 pub struct Transformation {
     new_tile_type: TileType,
 }
@@ -42,8 +42,25 @@ pub fn allowed_transformations_of_cell(cell: &Cell) -> Vec<Transformation> {
     new_tiles.iter().map(|tile| Transformation::to(*tile)).collect()
 }
 
-pub fn set_intersection(transformations_per_cell: Vec<Vec<Transformation>>) -> Vec<Transformation> {
-    Vec::new()
+pub fn set_intersection<T: PartialEq + Copy>(transformations_per_cell: Vec<Vec<T>>) -> Vec<T> {
+    if transformations_per_cell.len() == 0 {
+        Vec::new()
+    } else {
+        let mut result = Vec::new();
+        for should_be_present_in_all in transformations_per_cell.first().unwrap() {
+            let mut present = true;
+            for i in 1..transformations_per_cell.len() {
+                let transformations = &transformations_per_cell[i];
+                if !transformations.contains(should_be_present_in_all) {
+                    present = false;
+                }
+            }
+            if present {
+                result.push(*should_be_present_in_all);
+            }
+        }
+        result
+    }
 }
 
 impl Transformation {
@@ -56,6 +73,7 @@ impl Transformation {
 mod tests {
     use super::*;
     use crate::map::{Cell, TileType};
+    use crate::map::TileType::MachineDrill;
 
     #[test]
     fn test_basic_transformation() {
@@ -67,5 +85,22 @@ mod tests {
             transformation,
             vec![Transformation::to(TileType::MachineDrill)]
         );
+    }
+
+    #[test]
+    fn test_intersection() {
+        let a = vec![1, 2, 3, 6, 7];
+        let b = vec![4, 3, 5, 6, 8];
+        let in_both = set_intersection(vec![a, b]);
+        assert_eq!(in_both, vec![3, 6]);
+    }
+
+    #[test]
+    fn test_intersection_transformation() {
+        let to_drill = Transformation::to(MachineDrill);
+        let a = vec![to_drill];
+        let b = vec![to_drill];
+        let in_both = set_intersection(vec![a, b]);
+        assert_eq!(in_both, vec![to_drill]);
     }
 }
