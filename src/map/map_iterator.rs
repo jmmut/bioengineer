06d120afd91803d0d0b29c2ccd83ba9ce3f5299a
jose miguel mut <jomutlo@gmail.com>
2022-06-27 18:13:18 +0100

    allow empty CellIter

--- src/map/map_iterator.rs
+++ src/map/map_iterator.rs
@@ -21,7 +21,6 @@ pub struct MapIterator<'a> {
     chunks: &'a HashMap<ChunkIndex, Chunk>,
     chunk_iterator: Iter<'a, ChunkIndex, Chunk>,
     cell_iterator: CellIter<'a>,
-    empty: bool,
 }
 
 impl<'a> IntoIterator for &'a Map {
@@ -30,16 +29,14 @@ impl<'a> IntoIterator for &'a Map {
 
     fn into_iter(self) -> Self::IntoIter {
         let mut chunk_iterator = self.chunks.iter();
-        let chunk = chunk_iterator.next();
-        let empty = chunk.is_none();
-        let cell_iterator = chunk
+        let optional_chunk = chunk_iterator.next();
+        let cell_iterator = optional_chunk
             .map(|chunk| chunk.1.into_iter())
             .unwrap_or(CellIter::Empty);
         MapIterator::<'a> {
             chunks: &self.chunks,
             chunk_iterator,
             cell_iterator,
-            empty,
         }
     }
 }
@@ -48,20 +45,18 @@ impl Iterator for MapIterator<'_> {
     type Item = Cell;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.empty {
-            Option::None
-        } else {
+        let cell = self.cell_iterator.next();
+        cell.or_else(|| {
+            let optional_chunk = &mut self.chunk_iterator.next();
+            self.cell_iterator = optional_chunk
+                .map(|chunk| chunk.1.into_iter())
+                .unwrap_or(CellIter::Empty);
             let cell = self.cell_iterator.next();
-            cell.or_else(|| {
-                self.cell_iterator = (&mut self.chunk_iterator.next())
-                    .map(|chunk| chunk.1.into_iter())
-                    .unwrap_or(CellIter::Empty);
-                let cell = self.cell_iterator.next();
-                cell
-            })
-        }
+            cell
+        })
     }
 }
+
 
 #[cfg(test)]
 mod tests {
