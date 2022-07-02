5177e66252e4b679045b86b47e8ff40a1313e8e0
jose miguel mut <jomutlo@gmail.com>
2022-07-02 01:17:51 +0100

    more minor renamings in RefMutIterator

--- src/map/chunk.rs
+++ src/map/chunk.rs
@@ -109,21 +109,21 @@ impl CellIter {
     }
 }
 pub struct CellIterItem<'a> {
-    index: CellIndex,
+    cell_index: CellIndex,
     cell: &'a mut Cell,
 }
 
-pub trait MutRefIterator<'a, T> {
+pub trait RefMutIterator<'a, T> {
     fn next(&'a mut self) -> Option<T>;
 }
 
-impl<'a> MutRefIterator<'a, CellIterItem<'a>> for &mut CellIter {
+impl<'a> RefMutIterator<'a, CellIterItem<'a>> for &mut CellIter {
     fn next(&'a mut self) -> Option<CellIterItem<'a>> {
         self.i += 1;
         let i_usize = self.i as usize;
         return if i_usize < self.cells.len() {
             Option::Some(CellIterItem {
-                index: self.origin + get_cell_index(i_usize),
+                cell_index: self.origin + get_cell_index(i_usize),
                 cell: &mut (self.cells[i_usize])}
             )
         } else {
@@ -354,11 +354,10 @@ mod tests {
         let chunk = Chunk::new(CellIndex::new(0, 0, 0));
         let mut i = 0;
         let mut iter = chunk.into_iter();
-        let mut iter_mut = &mut iter;
         let mut sum_pressures = 0;
-        while let Option::Some(cell_iter_item) = iter_mut.next() {
-            cell_iter_item.cell.pressure += 1;
-            sum_pressures += cell_iter_item.cell.pressure;
+        while let Option::Some(CellIterItem{cell_index, cell}) = (&mut iter).next() {
+            cell.pressure += 1;
+            sum_pressures += cell.pressure;
             i += 1;
         }
         // iter.next();
@@ -376,7 +375,7 @@ mod tests {
     #[test]
     fn test_vec_iter() {
         let mut v = vec![1, 2, 3];
-        let x = (&mut v).into_iter();
+        let x = v.iter_mut();
         for n in x {
             *n += 10;
         }
