ccf707b2387fbbccd703cf8fa737fc4488ecae63
jose miguel mut <jomutlo@gmail.com>
2022-08-15 21:04:40 +0100

    update .clippy/run.sh

--- .clippy/run.sh
+++ .clippy/run.sh
@@ -1,1 +1,1 @@
-cargo clippy -- -A  clippy::collapsible_if  -A clippy::collapsible_else_if  -A clippy::len_zero -A clippy::let_and_return -A clippy::needless_return -A clippy::manual_range_contains
+cargo clippy --all-targets -- -A  clippy::collapsible_if  -A clippy::collapsible_else_if  -A clippy::len_zero -A clippy::let_and_return -A clippy::needless_return -A clippy::manual_range_contains -A clippy::bool_assert_comparison
