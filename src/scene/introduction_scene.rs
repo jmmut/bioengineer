8d5376e8219d6e03e06d64d3765940c688bd8a60
jose miguel mut <jomutlo@gmail.com>
2023-09-27 00:32:36 +0100

    extract draw_stars and draw_fire

--- src/scene/introduction_scene.rs
+++ src/scene/introduction_scene.rs
@@ -58,6 +58,83 @@ impl IntroductionScene {
     fn reset(&mut self) {
         *self = Self::new(self.drawer.take().unwrap())
     }
+
+    fn draw_stars(&mut self, height: f32, width: f32, rand: f32) {
+        if self.frame % 20 == 0 {
+            let rand2 = next_rand((self.frame as f32 * rand) as i64);
+            self.stars.push(Particle {
+                pos: Vec2::new(rand * width, height),
+                direction: Vec2::new(0.0, -STARS_SPEED - rand2 * 0.0625),
+                opacity: 1.0,
+            });
+        }
+        self.stars_opacity = (self.stars_opacity + 0.002).min(1.0);
+        let mut to_remove = Vec::new();
+        for (i, particle) in &mut self.stars.iter_mut().enumerate() {
+            particle.pos += particle.direction;
+            if particle.pos.x < 0.0 {
+                to_remove.push(i);
+            }
+            let rand2 = next_rand(i as i64 + (rand * 1000.0) as i64);
+            // particle.opacity
+            let mut white = Color::new(
+                1.0,
+                1.0,
+                1.0,
+                self.stars_opacity - self.stars_opacity * rand2 * 0.75,
+            );
+            // draw_circle(particle.pos.x, particle.pos.y, 1.5, white);
+
+            self.drawer.as_mut().unwrap().draw_rectangle(
+                particle.pos.x - 1.5,
+                particle.pos.y - 1.5,
+                3.0,
+                3.0,
+                white,
+            );
+            white.a *= 0.03;
+            draw_circle(particle.pos.x, particle.pos.y, 8.0, white);
+            // TODO: draw black circles to simulate a brightness cross
+        }
+
+        for i in to_remove.iter().rev() {
+            self.stars.swap_remove(*i);
+        }
+    }
+
+    fn draw_fire(&mut self, rand: f32) {
+        let pos_x = if self.frame % 2 == 0 {
+            ((rand - 0.5) * 2.0 + 5.0) * ZOOM
+        } else {
+            ((rand - 0.5) * 2.0 - 12.5) * ZOOM
+        };
+        self.fire.push(Particle {
+            pos: self.ship_pos + Vec2::new(pos_x, 5.0),
+            direction: Vec2::new(0.0, -3.0) + Vec2::new((rand - 0.5) * 2.0, 0.0),
+            opacity: 1.0,
+        });
+        let mut to_remove = Vec::new();
+        for (i, particle) in &mut self.fire.iter_mut().enumerate() {
+            particle.pos += particle.direction;
+            let particle_ship_diff = self.ship_pos - particle.pos;
+            particle.opacity = 1.0 - particle_ship_diff.length() / 200.0;
+            if particle.opacity < 0.0 {
+                to_remove.push(i);
+            }
+
+            let yellow = Color::new(0.8, 0.9, 0.5, 0.75 * particle.opacity);
+            self.drawer.as_mut().unwrap().draw_rectangle(
+                particle.pos.x,
+                particle.pos.y,
+                5.0,
+                5.0,
+                yellow,
+            );
+        }
+        for i in to_remove.iter().rev() {
+            self.fire.swap_remove(*i);
+        }
+    }
 }
 
 const ZOOM: f32 = 2.0;
@@ -68,9 +145,8 @@ impl Scene for IntroductionScene {
             self.reset();
         }
         self.frame = (self.frame + 1) % 100000000;
-        let drawer = self.drawer.as_mut().unwrap();
-        let height = drawer.screen_height();
-        let width = drawer.screen_width();
+        let height = self.drawer.as_mut().unwrap().screen_height();
+        let width = self.drawer.as_mut().unwrap().screen_width();
         let mut buttons = Vec::new();
         if self.ship_pos.y < height * 0.5 {
             self.ship_pos.y += 2.0;
@@ -106,78 +182,15 @@ impl Scene for IntroductionScene {
         if new_game_clicked || is_key_pressed(KeyCode::Escape) {
             State::ShouldFinish
         } else {
-            drawer.clear_background(BLACK);
+            self.drawer.as_mut().unwrap().clear_background(BLACK);
 
             let rand = next_rand(self.frame);
 
-            if self.frame % 20 == 0 {
-                let rand2 = next_rand((self.frame as f32 * rand) as i64);
-                self.stars.push(Particle {
-                    pos: Vec2::new(rand * width, height),
-                    direction: Vec2::new(0.0, -STARS_SPEED - rand2 * 0.0625),
-                    opacity: 1.0,
-                });
-            }
-            self.stars_opacity = (self.stars_opacity + 0.002).min(1.0);
-            let mut to_remove = Vec::new();
-            for (i, particle) in &mut self.stars.iter_mut().enumerate() {
-                particle.pos += particle.direction;
-                if particle.pos.x < 0.0 {
-                    to_remove.push(i);
-                }
-                let rand2 = next_rand(i as i64 + (rand * 1000.0) as i64);
-                // particle.opacity
-                let mut white = Color::new(
-                    1.0,
-                    1.0,
-                    1.0,
-                    self.stars_opacity - self.stars_opacity * rand2 * 0.75,
-                );
-                // draw_circle(particle.pos.x, particle.pos.y, 1.5, white);
+            self.draw_stars(height, width, rand);
 
-                drawer.draw_rectangle(
-                    particle.pos.x - 1.5,
-                    particle.pos.y - 1.5,
-                    3.0,
-                    3.0,
-                    white,
-                );
-                white.a *= 0.03;
-                // if particle.pos.x < width * 0.5 {
-                draw_circle(particle.pos.x, particle.pos.y, 8.0, white);
-                // }
-                // drawer
-                //     .draw_rectangle(particle.pos.x - 5.0, particle.pos.y-5.0, 13.0, 13.0, white);
-            }
-            for i in to_remove.iter().rev() {
-                self.stars.swap_remove(*i);
-            }
-            let pos_x = if self.frame % 2 == 0 {
-                ((rand - 0.5) * 2.0 + 5.0) * ZOOM
-            } else {
-                ((rand - 0.5) * 2.0 - 12.5) * ZOOM
-            };
-            self.fire.push(Particle {
-                pos: self.ship_pos + Vec2::new(pos_x, 5.0),
-                direction: Vec2::new(0.0, -3.0) + Vec2::new((rand - 0.5) * 2.0, 0.0),
-                opacity: 1.0,
-            });
-            let mut to_remove = Vec::new();
-            for (i, particle) in &mut self.fire.iter_mut().enumerate() {
-                particle.pos += particle.direction;
-                let particle_ship_diff = self.ship_pos - particle.pos;
-                particle.opacity = 1.0 - particle_ship_diff.length() / 200.0;
-                if particle.opacity < 0.0 {
-                    to_remove.push(i);
-                }
+            self.draw_fire(rand);
 
-                let yellow = Color::new(0.8, 0.9, 0.5, 0.75 * particle.opacity);
-                drawer
-                    .draw_rectangle(particle.pos.x, particle.pos.y, 5.0, 5.0, yellow);
-            }
-            for i in to_remove.iter().rev() {
-                self.fire.swap_remove(*i);
-            }
+            let drawer = self.drawer.as_mut().unwrap();
             let texture_size = drawer.texture_size(&ExtraTextures::Ship) * ZOOM;
             drawer.draw_rotated_texture(
                 &ExtraTextures::Ship,
